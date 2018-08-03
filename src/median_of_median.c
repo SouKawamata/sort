885ef4ad8c0211ad1dafc8acef968442b86de5d6
SouKawamata <kawamata.s.ac@m.titech.ac.jp>
2018-08-03 21:35:04 +0900

    Update_median

--- src/median_of_median.c
+++ src/median_of_median.c
@@ -9,11 +9,52 @@ int A[N];
  A[0], A[1], ..., A[n-1] の中でk+1番目に小さい値を返す関数
  ただし、Aの中身は書き換えてしまう。
  */
+
+void quick_sort(int A[], int n){
+    if(n <= 1) return;
+    int i, j, pivot;
+    pivot = A[0];
+    for(i = j = 1;i < n; i++){
+        if(A[i] <= pivot){
+            int z = A[j];
+            A[j] = A[i];
+            A[i] = z;
+            j++;
+        }
+    }
+
+    A[0] = A[j-1];
+    A[j-1] = pivot;
+    quick_sort(A, j-1);
+    quick_sort(A+j, n-j);
+}
+
 int quick_select(int A[], int n, int k){
-    int i, l, r, pivot;
+    //中央値の中央値を求めてA[0]に持ってくる。
+    if(n <= 5){
+      quick_sort(A,n);
+      return A[k];
+    }
+
+    int a,  D[N];
+    for(a = 0; a < n/5; a++){
+      D[a] = quick_select(A+5*a, 5, 2);
+    }
+    if(n%5 != 0) D[n/5] = quick_select(A+(n/5)*5, n%5, (n%5)/2);
+    int pivot = quick_select(D, (n+4)/5, ((n+4)/5)/2);
+
+    int z = A[0];
+    for (int i = 0; i < n; i++) {
+      if(A[i] == pivot){
+        A[0] = A[i];
+        A[i] = z;
+      }
+    }
+    //ここまで
+
+    int i, l, r;
     i = l = 1;
     r = n-1;
-    pivot = A[0];
     while(i <= r){
         if(A[i] < pivot){
             int z = A[l];
@@ -42,11 +83,11 @@ int main(){
     for(i=2;i<N;i++){
         A[i] = (long long int) A[i-1] * A[1] % N;
     }
-    
+
     // すべての要素が同じ場合でも計算が早く終わるか確認する
-    
+
     for(i=0;i<N;i++){
         if(quick_select(A, N, i) != i) printf("ERROR %d %d\n", i, quick_select(A, N, i));
-        //    printf("%d th element is %d\n", i, quick_select(A, N, i));
+            printf("%d th element is %d\n", i, quick_select(A, N, i));
     }
 }
